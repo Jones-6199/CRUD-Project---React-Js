4cb6c8d1e305494f78d89975ef5bda2d9152f07e
Jones-6199 <younesbenziane17@gmail.com>
2025-10-04 01:36:31 +0200

    Branche 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import TodoList from "./Components/TodoList";
 
 
 
@@ -13,8 +14,16 @@ export default function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" style={{
+      display:'flex',
+      alignItems:'center',
+      background:'#191b1f',
+      height:'100vh',
+      justifyContent:'center',
+      
 
+    }}>
+      <TodoList />
     </div>
   );
 }
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+
+
 
 export default function TodoList() {
+
+
+
+
+
+
   return (
-    <div>
+    
+      
+      <Container maxWidth="sm">
+     <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom style={{alignContent:'center',justifyContent:'center',display:'flex'}} variant='h2' sx={{ color: 'text.secondary', }}>
+        Day Plan
+        </Typography>
+        <Divider />
         
-    </div>
-  )
+        {/* This is toggle buttons */}
+
+       <ToggleButtonGroup
+      exclusive
+      aria-label="text alignment"
+      style={{
+        justifyContent:'center',
+        display:'flex',
+        marginTop:'20px',
+
+
+      }}
+    >
+      <ToggleButton value="left" >
+        General
+      </ToggleButton>
+
+      <ToggleButton value="center" >
+        Done 
+      </ToggleButton>
+
+      <ToggleButton value="right" >
+        Not Done yet
+      </ToggleButton>
+  
+    </ToggleButtonGroup>
+
+    {/* The ened of Tuggle Buttons */ }
+
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+      </Container>
+
+
+    
+  );
 }
